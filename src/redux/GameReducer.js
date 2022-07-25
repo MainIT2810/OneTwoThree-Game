9ca430201b3bbebcb843c3fd347239390ac7e06d
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-07-25 14:22:30 +0700

    Tạo stateDefault từ redux

--- src/redux/GameReducer.js
+++ src/redux/GameReducer.js
@@ -1,5 +1,18 @@
-const stateDefault = {};
+const stateDefault = {
+  ketQua: "I LOVE YOU 3000!!!",
+  soBanThang: 0,
+  tongSoManChoi: 0,
+  Player: [
+    { ma: "keo", hinhAnh: "./image/keo.png", datCuoc: false },
+    { ma: "bua", hinhAnh: "./image/bua.png", datCuoc: false },
+    { ma: "bao", hinhAnh: "./image/bao.png", datCuoc: false },
+  ],
 
-const GameXucXacReducer = (state = stateDefault, action) => {};
+  computer: { ma: "keo", hinhAnh: "./image/keo.png" },
+};
 
-export default GameXucXacReducer;
+const GameReducer = (state = stateDefault, action) => {
+  return { ...state };
+};
+
+export default GameReducer;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,7 @@
 import {combineReducers} from "redux";
-
+import GameReducer from "./GameReducer";
 const rootReducer = combineReducers({ //Store tổng của ứng dụng
-   
+    GameReducer //store của Game Kéo Búa Bao
 
 });
 
