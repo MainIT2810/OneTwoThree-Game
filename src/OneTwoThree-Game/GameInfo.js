b976e5e836f00acf17614607a53db567d0d8dc65
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-07-25 14:29:30 +0700

    Binding dữ liệu từ redux về component GameInfo

--- src/OneTwoThree-Game/GameInfo.js
+++ src/OneTwoThree-Game/GameInfo.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react'
-
-export default class GameInfo extends Component {
+import {connect} from "react-redux"
+class GameInfo extends Component {
   render() {
     return (
       <div>
         <div className="display-5 mb-3 text-warning">
-          I love you 3000! 
+          {this.props.ketQua}
         </div>
 
         <div className="display-5 text-success">
-          Số bàn thắng: <span>0</span>
+          Số bàn thắng: <span>{this.props.soBanThang}</span>
         </div>
 
         <div className="display-5 text-success">
-        Tổng số bàn chơi: <span>0</span>
+        Tổng số bàn chơi: <span>{this.props.tongSoManChoi}</span>
         </div>
 
       </div>
     )
   }
 }
+
+const mapStateToProps = state => {
+  return({
+    ketQua: state.GameReducer.ketQua,
+    soBanThang: state.GameReducer.soBanThang,
+    tongSoManChoi: state.GameReducer.tongSoManChoi,
+  });
+}
+
+export default connect(mapStateToProps)(GameInfo);
