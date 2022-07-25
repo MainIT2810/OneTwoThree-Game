e16cef40852d784c8d8f2853be95a864fd917cf2
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-07-25 15:23:28 +0700

    Làm chức năng đặt cược

--- src/OneTwoThree-Game/Player.js
+++ src/OneTwoThree-Game/Player.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 class Player extends Component {
   renderItem = () => {
-    return this.props.player.map((item, index) => {
+    return this.props.BetArr.map((item, index) => {
       let border = {};
       if (item.datCuoc) {
         border = { border: "3px solid orange" };
       }
       return (
         <div className="col-4" key={index}>
-          <button style={border} className="btn btn-success">
+          <button
+            style={border}
+            className="btn btn-success"
+            onClick={() => {
+              this.props.datCuoc(item.ma);
+            }}
+          >
             <img src={item.hinhAnh} alt="item" width={50} height={50} />
           </button>
         </div>
@@ -23,14 +29,14 @@ class Player extends Component {
         <div className="text-center playerGame mt-5">
           <div className="theThink">
             <img
-              src={this.props.player.find((item) => item.datCuoc === true).hinhAnh}
-              alt="item"
+              style={{ transform: "rotate(120deg)" }}
+              className="mt-3"
               width={100}
               height={100}
-              className="mt-3"
+              src={this.props.BetArr.find(item => item.datCuoc === true).hinhAnh}
+              alt= "item"
             />
           </div>
-
           <div className="speech-bubble"></div>
           <img src="./image/player.png" alt="player" width={125} height={150} />
 
@@ -43,8 +49,19 @@ class Player extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    player: state.GameReducer.Player,
+    BetArr: state.GameReducer.BetArr,
   };
 };
 
-export default connect(mapStateToProps)(Player);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    datCuoc: (maCuoc) => {
+      dispatch({
+        type: "CHON_KEO_BUA_BAO",
+        maCuoc,
+      });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
--- src/redux/GameReducer.js
+++ src/redux/GameReducer.js
@@ -2,7 +2,7 @@ const stateDefault = {
   ketQua: "I LOVE YOU 3000!!!",
   soBanThang: 0,
   tongSoManChoi: 0,
-  Player: [
+  BetArr: [
     { ma: "keo", hinhAnh: "./image/keo.png", datCuoc: true },
     { ma: "bua", hinhAnh: "./image/bua.png", datCuoc: false },
     { ma: "bao", hinhAnh: "./image/bao.png", datCuoc: false },
@@ -12,6 +12,27 @@ const stateDefault = {
 };
 
 const GameReducer = (state = stateDefault, action) => {
+  switch (action.type) {
+    case "CHON_KEO_BUA_BAO": {
+      // Reset player
+      let PlayerBetUpdate = [...state.BetArr];
+
+      // Tạo ra mảng BetArr mới từ BetArr cũ và action. maCuoc do user dispatch lên
+
+      PlayerBetUpdate = PlayerBetUpdate.map((item, index) => {
+        if (item.ma === action.maCuoc) {
+          return { ...item, datCuoc: true };
+        }
+        return { ...item, datCuoc: false };
+      });
+      // Cập nhật state player = PlayerBetUpdate
+      state.BetArr = PlayerBetUpdate;
+      return { ...state };
+    }
+
+    default:
+      break;
+  }
   return { ...state };
 };
 
